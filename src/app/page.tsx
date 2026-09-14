ec87d40951087444e37a5f6c231a9ed2a2a2aa2f
zahra <zahrahanifi98@gmail.com>
2026-09-14 16:26:24 +0330

    Add lights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,11 @@ import Reveal from "@/components/Reveal";
 export default function Home() {
   return (
       <>
+          <div aria-hidden className="pointer-events-none absolute inset-x-0 top-0 -z-10 h-280 overflow-hidden">
+              <div className="absolute top-22 left-[max(0px,calc(50%-32rem))] size-180 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,rgb(110_139_255/0.35),transparent_70%)]" />
+              <div className="absolute top-200 right-[max(0px,calc(50%-30rem))] size-120 translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,rgb(90_214_192/0.15),transparent_70%)]" />
+          </div>
+
           <IntroSection />
 
           <Reveal>
